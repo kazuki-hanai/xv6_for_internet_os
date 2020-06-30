24ff5ef47e3ff5e444c50cdde01ae019e5b8225b
wan-nyan-wan <distributed.system.love@gmail.com>
2020-06-30 11:41:36 +0900

    fix test to check all sizes

--- kernel/test/lib/test_buddy.c
+++ kernel/test/lib/test_buddy.c
@@ -8,19 +8,36 @@ extern uint64 bd_allocated;
 
 void buddy_test() {
   if (cpuid() == 0) {
-
-    // ticksleep(10);
-    printf("\t[buddy test] start...");
+    printf("\t[buddy test] start...\n");
     // page alloc
+
+    // 64
+    for (int n = 0; n < 2; n++) {
+      void *pages_64[MAX_PAGES << 7];
+      for (int i = 0; i < (MAX_PAGES-1) * 32; i++) {
+        pages_64[i] = bd_alloc(64);
+        if (pages_64[i] == 0) {
+          printf("n: %d, i: %d\n", n, i);
+          panic("bd_alloc failed!");
+        }
+        memset(pages_64[i], n % 16, 64);
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 32; i++) {
+        for (int j = 0; j < 64; j++) {
+          if ((((char *)pages_64[i])[j] != n % 16)) {
+            panic("\nbuddy test failed...");
+          }
+        }
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 32; i++) {
+        bd_free(pages_64[i]);
+      }
+    }
+    printf("\t\t64...done!\n");
+
+    // 4096
     for (int n = 0; n < 100; n++) {
       void *pages_4096[MAX_PAGES];
-      void *pages_2048[MAX_PAGES << 1];
-      void *pages_1024[MAX_PAGES << 2];
-      // void *pages_512[MAX_PAGES << 3];
-      // void *pages_256[MAX_PAGES << 4];
-      // void *pages_128[MAX_PAGES << 5];
-      // void *pages_64[MAX_PAGES << 6];
-
       for (int i = 0; i < MAX_PAGES-1; i++) {
         pages_4096[i] = bd_alloc(PGSIZE);
         if (pages_4096[i] == 0) {
@@ -39,8 +56,12 @@ void buddy_test() {
       for (int i = 0; i < MAX_PAGES-1; i++) {
         bd_free(pages_4096[i]);
       }
+    }
+    printf("\t\t4096...done!\n");
 
-      // 2048
+    // 2048
+    for (int n = 0; n < 100; n++) {
+      void *pages_2048[MAX_PAGES << 1];
       for (int i = 0; i < (MAX_PAGES-1) * 2; i++) {
         pages_2048[i] = bd_alloc(2048);
         if (pages_2048[i] == 0) {
@@ -59,8 +80,12 @@ void buddy_test() {
       for (int i = 0; i < (MAX_PAGES-1) * 2; i++) {
         bd_free(pages_2048[i]);
       }
+    }
+    printf("\t\t2048...done!\n");
 
-      // 1024
+    // 1024
+    for (int n = 0; n < 30; n++) {
+      void *pages_1024[MAX_PAGES << 2];
       for (int i = 0; i < (MAX_PAGES-1) * 4; i++) {
         pages_1024[i] = bd_alloc(1024);
         if (pages_1024[i] == 0) {
@@ -79,47 +104,82 @@ void buddy_test() {
       for (int i = 0; i < (MAX_PAGES-1) * 4; i++) {
         bd_free(pages_1024[i]);
       }
-
-      // 512
-      // for (int i = 0; i < (MAX_PAGES-1) * 8; i++) {
-      //   pages_512[i] = bd_alloc(512);
-      //   if (pages_512[i] == 0) {
-      //     printf("n: %d, i: %d\n", n, i);
-      //     panic("bd_alloc failed!");
-      //   }
-      //   memset(pages_512[i], n % 16, 512);
-      // }
-      // for (int i = 0; i < (MAX_PAGES-1) * 8; i++) {
-      //   for (int j = 0; j < 512; j++) {
-      //     if ((((char *)pages_512[i])[j] != n % 16)) {
-      //       panic("\nbuddy test failed...");
-      //     }
-      //   }
-      // }
-      // for (int i = 0; i < (MAX_PAGES-1) * 8; i++) {
-      //   bd_free(pages_512[i]);
-      // }
-
-      // 256
-      // for (int i = 0; i < (MAX_PAGES-1) * 16; i++) {
-      //   pages_256[i] = bd_alloc(256);
-      //   if (pages_256[i] == 0) {
-      //     printf("n: %d, i: %d\n", n, i);
-      //     panic("bd_alloc failed!");
-      //   }
-      //   memset(pages_256[i], n % 16, 256);
-      // }
-      // for (int i = 0; i < (MAX_PAGES-1) * 16; i++) {
-      //   for (int j = 0; j < 256; j++) {
-      //     if ((((char *)pages_256[i])[j] != n % 16)) {
-      //       panic("\nbuddy test failed...");
-      //     }
-      //   }
-      // }
-      // for (int i = 0; i < (MAX_PAGES-1) * 16; i++) {
-      //   bd_free(pages_256[i]);
-      // }
     }
+    printf("\t\t1024...done!\n");
+
+    // 512
+    for (int n = 0; n < 10; n++) {
+      void *pages_512[MAX_PAGES << 3];
+      for (int i = 0; i < (MAX_PAGES-1) * 8; i++) {
+        pages_512[i] = bd_alloc(512);
+        if (pages_512[i] == 0) {
+          printf("n: %d, i: %d\n", n, i);
+          panic("bd_alloc failed!");
+        }
+        memset(pages_512[i], n % 16, 512);
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 8; i++) {
+        for (int j = 0; j < 512; j++) {
+          if ((((char *)pages_512[i])[j] != n % 16)) {
+            panic("\nbuddy test failed...");
+          }
+        }
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 8; i++) {
+        bd_free(pages_512[i]);
+      }
+    }
+    printf("\t\t512...done!\n");
+
+    // 256
+    for (int n = 0; n < 10; n++) {
+      void *pages_256[MAX_PAGES << 4];
+      for (int i = 0; i < (MAX_PAGES-1) * 16; i++) {
+        pages_256[i] = bd_alloc(256);
+        if (pages_256[i] == 0) {
+          printf("n: %d, i: %d\n", n, i);
+          panic("bd_alloc failed!");
+        }
+        memset(pages_256[i], n % 16, 256);
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 16; i++) {
+        for (int j = 0; j < 256; j++) {
+          if ((((char *)pages_256[i])[j] != n % 16)) {
+            panic("\nbuddy test failed...");
+          }
+        }
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 16; i++) {
+        bd_free(pages_256[i]);
+      }
+    }
+    printf("\t\t256...done!\n");
+
+    // 128
+    for (int n = 0; n < 2; n++) {
+      void *pages_128[MAX_PAGES << 6];
+      for (int i = 0; i < (MAX_PAGES-1) * 32; i++) {
+        pages_128[i] = bd_alloc(128);
+        if (pages_128[i] == 0) {
+          printf("n: %d, i: %d\n", n, i);
+          panic("bd_alloc failed!");
+        }
+        memset(pages_128[i], n % 16, 128);
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 32; i++) {
+        for (int j = 0; j < 128; j++) {
+          if ((((char *)pages_128[i])[j] != n % 16)) {
+            panic("\nbuddy test failed...");
+          }
+        }
+      }
+      for (int i = 0; i < (MAX_PAGES-1) * 32; i++) {
+        bd_free(pages_128[i]);
+      }
+    }
+    printf("\t\t128...done!\n");
+
+
     printf("finish! \n");
   }
 }
