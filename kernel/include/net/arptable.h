ff69b5ed7ea5634bea89d2bd2e53f52dd744ecb4
Inuha <distributed.system.love@gmail.com>
2020-06-30 14:34:03 +0900

    fix arptable(hash table) and tcbtable(hash table)

--- kernel/include/net/arptable.h
+++ kernel/include/net/arptable.h
@@ -1,17 +1,19 @@
 
 #define ARP_DEFUALT_ENTRY_NUM 32
 
-struct arp_entry {
+struct arp_cache {
   uint32 ip;
   uint8 mac[ETHADDR_LEN];
+  struct arp_cache *prev;
+  struct arp_cache *next;
 };
 
-struct arp_table {
+struct arp_cache_entry {
   struct spinlock lock;
-  struct arp_entry entries[ARP_DEFUALT_ENTRY_NUM];
+  struct arp_cache *head;
 };
 
 void arptable_init();
 void arptable_add(uint32, uint8*);
-int arptable_get(uint32, uint8*);
+void arptable_get_mac(uint32, uint8*);
 void arptable_del(uint32);
