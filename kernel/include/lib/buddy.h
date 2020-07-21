d0715a45c2b3c214e059f7b92f21892ea0af0ed1
wan-nyan-wan <distributed.system.love@gmail.com>
2020-07-21 17:50:57 +0900

    fix buddy page size

--- kernel/include/lib/buddy.h
+++ kernel/include/lib/buddy.h
@@ -5,6 +5,6 @@
 #define BLK_SIZE(k)   ((1L << (k)) * LEAF_SIZE) 
 #define HEAP_SIZE     BLK_SIZE(MAX_SIZE) // 4096
 #define ROUNDUP(n,sz) (((((n)-1)/(sz))+1)*(sz))
-#define MAX_PAGES     4
+#define MAX_PAGES     128
 
 void buddy_show_map();
