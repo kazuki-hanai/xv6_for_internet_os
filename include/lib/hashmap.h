01828fed7808bcb1f8e3b1560cab979b238b2996
wan-nyan-wan <distributed.system.love@gmail.com>
2020-10-07 17:04:18 +0900

    fix indent

--- include/lib/hashmap.h
+++ include/lib/hashmap.h
@@ -10,12 +10,12 @@
 struct hashmap;
 
 struct hashmap *hashmap_new(size_t elsize, size_t cap, 
-                            uint64_t seed0, uint64_t seed1,
-                            uint64_t (*hash)(const void *item, 
-                                             uint64_t seed0, uint64_t seed1),
-                            int (*compare)(const void *a, const void *b, 
-                                           void *udata),
-                            void *udata);
+                uint64_t seed0, uint64_t seed1,
+                uint64_t (*hash)(const void *item, 
+                                 uint64_t seed0, uint64_t seed1),
+                int (*compare)(const void *a, const void *b, 
+                               void *udata),
+                void *udata);
 void hashmap_free(struct hashmap *map);
 size_t hashmap_count(struct hashmap *map);
 bool hashmap_oom(struct hashmap *map);
@@ -24,9 +24,9 @@ void *hashmap_set(struct hashmap *map, void *item);
 void *hashmap_delete(struct hashmap *map, void *item);
 void *hashmap_probe(struct hashmap *map, uint64_t position);
 bool hashmap_scan(struct hashmap *map,
-                  bool (*iter)(const void *item, void *udata), void *udata);
+                bool (*iter)(const void *item, void *udata), void *udata);
 void hashmap_set_allocator(void *(*malloc)(int), void (*free)(void*));
 uint64_t hashmap_sip(const void *data, size_t len, 
-                     uint64_t seed0, uint64_t seed1);
+                uint64_t seed0, uint64_t seed1);
 uint64_t hashmap_murmur(const void *data, size_t len, 
-                        uint64_t seed0, uint64_t seed1);
+                uint64_t seed0, uint64_t seed1);
