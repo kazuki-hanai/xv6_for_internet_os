ecaf5d84324b11a633d0c9a5b6311a0e5c32924c
wan-nyan-wan <distributed.system.love@gmail.com>
2020-11-11 14:11:48 +0900

    sys_addnode, sys_removenode

--- kernel/sys/sysremote.c
+++ kernel/sys/sysremote.c
@@ -1,13 +1,41 @@
 #include "types.h"
+#include "defs.h"
+#include "nodes.h"
 
 uint64_t sys_getnodes(void) {
+	uint64_t addr;
+	if (argaddr(0, &addr) < 0) {
+		return -1;
+	}
+	return 0;
+}
+
+uint64_t sys_getnode(void) {
 	return 0;
 }
 
 uint64_t sys_addnode(void) {
+	uint64_t nid;
+	if (arguint64(0, &nid) < 0) {
+		return -1;
+	}
+
+	if (node_add(nid) < 0) {
+		return -1;
+	}
+
 	return 0;
 }
 
 uint64_t sys_removenode(void) {
+	uint64_t nid;
+	if (arguint64(0, &nid) < 0) {
+		return -1;
+	}
+
+	if (node_remove(nid) < 0) {
+		return -1;
+	}
+
 	return 0;
 }
